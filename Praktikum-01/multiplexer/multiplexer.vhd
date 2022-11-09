299f10baf974d9837bea0eeed4bd641bdab5cf5d
martin <exze.real@gmail.com>
2022-11-09 01:53:03 +0100

    Update multiplexer.vhd

--- Praktikum-01/multiplexer/multiplexer.vhd
+++ Praktikum-01/multiplexer/multiplexer.vhd
@@ -1,23 +1,27 @@
 library ieee;
 use ieee.std_logic_1164.all;
- 
+
+
 entity multiplexer is
 	port(
-		SW: 	in std_logic_vector(7 downto 0);
-		PB: 	in std_logic_vector(2 downto 0);
-		LED: 	out std_logic
+		A:		in std_logic_vector(7 downto 0);			-- zu multiplexende Eingangssignale Pin-Belegung beginnend bei A0: SW0 rechter Schalter bis A7: SW7 dritter Schalter von links
+		S:		in std_logic_vector(2 downto 0);			-- Adresse
+		X:		out std_logic -- Output auf LED
 	);
+
 end multiplexer;
- 
+
+
+
 architecture verhalten of multiplexer is
 begin
-	with PB select
-		LED <= 	SW(0) when "000",
-					SW(1) when "001",
-					Sw(2) when "010",
-					SW(3) when "011",
-					SW(4) when "100",
-					SW(5) when "101",
-					SW(6) when "110",
-					SW(7) when "111";
+	with S select
+		X <= 		A(0) when "000",
+					A(1) when "001",
+					A(2) when "010",
+					A(3) when "011",
+					A(4) when "100",
+					A(5) when "101",
+					A(6) when "110",
+					A(7) when "111";
 end verhalten;
